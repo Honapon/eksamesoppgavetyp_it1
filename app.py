04fa25a99cb61261c1ba9fd1f24fff1ce2b4b30b
Honapona_ <156892939+Honapon@users.noreply.github.com>
2025-05-12 22:10:39 +0200

    utkast 1

--- app.py
+++ app.py
@@ -29,54 +29,62 @@ def registrer():
 
 @app.route('/submit_reg', methods=['POST'])
 def submit_reg():
-    #tar input fra form. 
-    first_name = request.form['fornavn']
-    last_name = request.form['etternavn']
-    email = request.form['email']
-    phone_number = request.form['tlf']
-    address = request.form['adresse']
-    city = request.form['by']
-    country = request.form['land']
-
-    vehicle_type = request.form['kjoretoy']
-    make = request.form['merke']
-    model = request.form['modell']
-    year = request.form['aar']
-    registration_number = request.form['reg']
-    
-    event_year = request.form['eventYear'] 
-    comments = request.form['comments'] 
-    #validate reqired fields
-    if not all([first_name, last_name, email, event_year]):
-        return "fyll ut fornavn, etternavn, email og event år", 400
-    
     try:
+        #tar input fra form. 
+        first_name = request.form['fornavn']
+        last_name = request.form['etternavn']
+        email = request.form['email']
+        phone_number = request.form['tlf']
+        address = request.form['adresse']
+        city = request.form['by']
+        country = request.form['land']
+
+        vehicle_type = request.form['kjoretoy']
+        make = request.form['merke']
+        model = request.form['modell']
+        year = request.form['aar']
+        registration_number = request.form['reg']
+    
+        event_year = request.form['eventYear'] 
+        comments = request.form['comments'] 
+    #validate reqired fields
+        if not all([first_name, last_name, email, event_year]):
+            return "fyll ut fornavn, etternavn, email og event år", 400
+    
+    
         connection = db_connection()
         cursor = connection.cursor()
         
+        check_participant_query = "SELECT ParticipantID FROM Participants WHERE Email = %s"
+        cursor.execute(check_participant_query, (email,))
+        participant_result = cursor.fetchone()
+        
+        if participant_result:
+            participant_id = participant_result[0]
+        else:
         #insert deltaker data
-        participant_query = """
+            participant_query = """
         INSERT INTO Participants (FirstName, LastName, Email, PhoneNumber, Address, City, Country)
         VALUES (%s, %s, %s, %s, %s, %s, %s)
         """
         
-        participant_data = (first_name, last_name, email, phone_number, address, city, country)
-        cursor.execute(participant_query, participant_data)
-        participant_id = cursor.lastrowid
+            participant_data = (first_name, last_name, email, phone_number, address, city, country)
+            cursor.execute(participant_query, participant_data)
+            participant_id = cursor.lastrowid
         
         
-        # insert kjøretøy data
+    # insert kjøretøy data
         vehicle_query = """
-        INSERT INTO Vehicles (ParticipantID, VehicleType, Make, Model, Year, RegistrationNumber)
-        VALUES (%s, %s, %s, %s, %s, %s)
+    INSERT INTO Vehicles (ParticipantID, VehicleType, Make, Model, Year, RegistrationNumber)
+    VALUES (%s, %s, %s, %s, %s, %s)
         """
         vehicle_data = (participant_id, vehicle_type, make, model, year, registration_number)
         cursor.execute(vehicle_query, vehicle_data)
         
         # participation record
         participation_record_query = """
-        INSERT INTO ParticipationRecords (ParticipantID, EventYear, Comments)
-        VALUES (%s, %s, %s)
+    INSERT INTO ParticipationRecords (ParticipantID, EventYear, Comments)
+    VALUES (%s, %s, %s)
         """
         participation_record_data = (participant_id, event_year, comments)
         cursor.execute(participation_record_query, participation_record_data)
